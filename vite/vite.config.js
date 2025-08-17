2699525007a5d73cc47f31daf4200ee38cf02920
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-08-17 19:09:03 +0530

    Update vite.config.js

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    port:3001,
+  }
 })
+
+// you can change the port no using 
+// server:{
+  //   port:(Your Choosen Port No),
+  // }
+// using here 3001 
