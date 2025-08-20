e681dc7413a88e0483ed0118bdc858ee9ec65163
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-08-20 20:03:20 +0530

    Update App.jsx

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,1 +1,12 @@
+import { Component, useState } from 'react'
+import './App.css'
 
+
+function Components() {
+  return (
+    <div>
+     <h1>hello this is component</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, autem?</p>
+    </div>
+  )
+}
