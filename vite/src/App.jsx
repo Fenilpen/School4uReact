ca992e68c74726b8b13d3e5c5ad836a9b4247586
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-08-21 17:39:11 +0530

    Update App.jsx

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,6 +1,24 @@
 import { Component, useState } from 'react'
 import './App.css'
 
+// function Component2 () {
+//   return(
+//     <div>
+//       <h1>this is component no 2</h1>
+//       <img src="https://tse1.mm.bing.net/th/id/OIP.GQWe6h-gMUSf7kMVzSBnTQHaF7?r=0&rs=1&pid=ImgDetMain&o=7&rm=3" alt="" srcset="" />
+//     </div>
+//   )
+// }
+
+// function Components() {
+//   return (
+//     <div>
+//      <h1>hello this is component</h1>
+//      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, autem?</p>
+//     </div>
+//   )
+// }
+
 
 function Components() {
   return (
