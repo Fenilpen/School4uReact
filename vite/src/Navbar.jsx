aa7f5d7b14d4cde61a9a873ba8ef6eb2ad6634d0
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-08-22 18:04:33 +0530

    Update Navbar.jsx

--- vite/src/Navbar.jsx
+++ vite/src/Navbar.jsx
@@ -1,1 +1,18 @@
-//
+import React from 'react'
+
+
+const Navim = ({title,link,number}) => {
+  // console.log(props);
+  
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h1>{link}</h1>
+      <h1>{number}</h1>
+      <h1>{number}</h1>
+    </div>
+  )
+}
+
+
+export default Navim
