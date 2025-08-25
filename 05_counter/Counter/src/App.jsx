f429a8202decdaa39acb4af9cf2f7a36180da858
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-08-25 18:07:53 +0530

    Update App.jsx

--- 05_counter/Counter/src/App.jsx
+++ 05_counter/Counter/src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [count, setCount] = useState(0)
 
   function increment () {
   count = count + 1
   setCount(count)
-
+  }
 
   return (
     <>
