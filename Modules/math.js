e09919f9e1e48537ce95ee8fa35efe23b2d58f16
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-09-04 17:55:43 +0530

    Update math.js

--- Modules/math.js
+++ Modules/math.js
@@ -2,5 +2,12 @@
 let name = "working on common js exports"
 let age = 25
 
+// lets try for functions
+
+function sum (a,b) {
+    return a + b
+}
+
+
 // to export
-module.exports = {name,age}
+module.exports = {name,age,sum}
