7ff22c30c44f283ba9ea94c6abac65a1691ad6fb
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-09-07 19:08:08 +0530

    Update script.js

--- Modules/script.js
+++ Modules/script.js
@@ -1,12 +1,1 @@
-//to import
-// let nm = require('./math.js')
-// let obj = require('./math.js')
 
-// console.log(obj.name)
-// console.log(obj.age)
-
-let {sum,name} = require("./math.js")
-
-let result = sum(1,2) 
-console.log(result);
-console.log(name);
