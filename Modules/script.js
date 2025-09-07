f8d0b9eee6c9913f2c979e9ceb35b14d761292be
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-09-07 19:20:38 +0530

    Update script.js

--- Modules/script.js
+++ Modules/script.js
@@ -15,8 +15,18 @@
 // import funky from"./math.js"
 // console.log(funky(5,5))
 
-import {sum,sub} from"./math.js"
+// import {sum,sub} from"./math.js"
 
-let result = sum(3,1)
-let result2 = sub(3,2)
-console.log(result,result2);
+// let result = sum(3,1)
+// let result2 = sub(3,2)
+// console.log(result,result2);
+
+
+
+// example of destructuring using as
+
+import {obj as CN} from"./math.js"
+
+let {name,age,work} = CN
+
+console.log(name,age,work);
