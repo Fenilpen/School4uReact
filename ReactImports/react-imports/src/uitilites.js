2438a5c2d45977887084d1fdc277bdf40466c76a
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-09-16 19:20:25 +0530

    Update uitilites.js

--- ReactImports/react-imports/src/uitilites.js
+++ ReactImports/react-imports/src/uitilites.js
@@ -1,7 +1,10 @@
 function Displayname () {
     let Hisname = "Zobra D Crepo"
     console.log(Hisname);
-    
+}
+
+export function SuperSum (a,b) {
+    return a + b
 }
 
 export default Displayname
