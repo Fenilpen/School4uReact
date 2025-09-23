05f96227d9da40b567173db9527c0eac4b857344
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-09-23 21:45:13 +0530

    Update App.jsx

--- JSX/src/App.jsx
+++ JSX/src/App.jsx
@@ -7,8 +7,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <> 
-    
+    <>
+    <h1>helloww  jsx </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptas. 
+    Temporibus accusantium quam sapiente voluptatibus impedit. Quam maiores, velit possimus esse pariatur rem eligendi cupiditate adipisci voluptas, enim exercitationem architecto!
+    A, consequuntur cum impedit quam numquam expedita quisquam in incidunt dolorem sunt necessitatibus tempore suscipit dolor cumque laudantium harum perspiciatis accusantium voluptas officiis pariatur modi? Ullam sunt ea maxime deleniti?
+    Fugiat nostrum, nulla placeat sequi soluta pariatur consequatur </p>
     </>
   )
 }
