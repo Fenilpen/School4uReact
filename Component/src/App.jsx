fe667b6ab3d654bdcafae1dfe1185edc757071d5
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-10-15 20:42:40 +0530

    Update App.jsx

--- Component/src/App.jsx
+++ Component/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
-
 import './App.css'
+
+function Square(){
+  return(
+    <div className="Square">here</div>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    <h1>hello react pe react</h1>
+    <Square/>
     </>
   )
 }
