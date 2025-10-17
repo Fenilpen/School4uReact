a399db1d9e8968c1668bcbe850028936273408dc
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-10-17 20:39:16 +0530

    Update App.jsx

--- Component/src/App.jsx
+++ Component/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 
+// as a function
+
 // function square(para1,para2){
 //   return(
 //     <>
@@ -12,12 +14,13 @@ import './App.css'
 //   )
 // }
 
-function Square(para1,para2){
+// as a component
+function Square(props){
   return(
     <>
     <div className="Square">
-    <h1>{para1}</h1>
-    <h1>{para2}</h1>
+    <h1>{props.name}</h1>
+    <h1>{props.age}</h1>
     </div>
     </>
   )
