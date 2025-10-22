f59cde382347001b24a0c20f33ff35da3243fabb
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-10-22 18:10:32 +0530

    Update Card.jsx

--- Component/src/Components/Card.jsx
+++ Component/src/Components/Card.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const Card = (props) => {
+  const [count,setCount] = useState(0)
+
+  function Increment(){
+    setCount(count + 1)
+  }
+  
   return (
     <div class="card">
       <h1>{props.title}</h1>
