f9e1bf859ec3d738adb543f69055c3de74a88ce8
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-10-24 15:51:50 +0530

    Update App.jsx

--- Component/src/App.jsx
+++ Component/src/App.jsx
@@ -3,6 +3,30 @@ import './App.css'
 import Card from './Components/Card'
 
 
+// as a function
+// function square(para1,para2){
+//   return(
+//     <>
+//     <div className="Square">
+//     <h1>{para1}</h1>
+//     <h1>{para2}</h1>
+//     </div>
+//     </>
+//   )
+// }
+
+
+// as a component
+// function Square(props){
+//   return(
+//     <>
+//     <div className="Square">
+//     <h1>{props.name}</h1>
+//     <h1>{props.age}</h1>
+//     </div>
+//     </>
+//   )
+// }
 
 
 // as a class
