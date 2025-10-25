6d7a5290c652fae20bb1f67130fe639bc348974f
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-10-25 17:00:36 +0530

    Add Card component with button and text

---
+++ vite-css/src/card.jsx
@@ -0,0 +1,13 @@
+import "./Card.css"
+function Card() {
+  return (
+    <>
+    <button className='btn'>card</button>
+    <p className='Text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, itaque odit non, quaerat fugit ipsam accusamus illo dicta neque, consectetur ut totam provident dolor error accusantium laboriosam ipsum voluptatum? Veritatis.
+    Quisp</p>
+    </>
+
+  )
+}
+
+export default Card
