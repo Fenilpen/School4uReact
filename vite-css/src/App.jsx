f334edc496cb6ec0274cc913191162fdd90e2473
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-10-28 16:17:11 +0530

    Update App.jsx

--- vite-css/src/App.jsx
+++ vite-css/src/App.jsx
@@ -13,3 +13,15 @@ function App() {
 }
 
 export default App
+
+
+// css in react
+
+// 1. regular css
+// if you import css file in react those styles are global by default
+
+// 2. css module
+// lets you scope css to a component
+
+// 3. inline css
+// you use style prop, which takes a javascript object (not a string)
