a55faf8e65b5cdb357ee0095ac94825d1a279050
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-10-28 16:28:32 +0530

    Update card.jsx

--- vite-css/src/card.jsx
+++ vite-css/src/card.jsx
@@ -1,9 +1,10 @@
-import "./Card.css"
+import style from "./Card.module.css"
+
 function Card() {
   return (
     <>
     <button className='btn'>card</button>
-    <p className='Text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, itaque odit non, quaerat fugit ipsam accusamus illo dicta neque, consectetur ut totam provident dolor error accusantium laboriosam ipsum voluptatum? Veritatis.
+    <p className={style.Text}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, itaque odit non, quaerat fugit ipsam accusamus illo dicta neque, consectetur ut totam provident dolor error accusantium laboriosam ipsum voluptatum? Veritatis.
     Quisp</p>
     </>
 
