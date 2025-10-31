7a76e1e1bc2c67860c901aebc9d5cfae443a7c7b
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-10-31 16:20:24 +0530

    Update App.jsx

--- Tailwind-css/src/App.jsx
+++ Tailwind-css/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import "./Card.css"
+import Card from './Card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
+    <> 
+    <h1>hellow how are you</h1>
+    <Card/>
     </>
   )
 }
