965719600f38c07f54c2851d37c7d8d7b6dc36f9
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-04 18:41:39 +0530

    Update App.jsx

--- Tailwind-css/src/App.jsx
+++ Tailwind-css/src/App.jsx
@@ -6,8 +6,6 @@ function App() {
 
   return (
     <> 
-    <h1 className='bg-blue-600'>hellow how are you</h1>
-    <button className='bg-[lightseagreen] rounded-[7px] pt-10 pb-8 pr-6 pl-5'>click me</button>
     </>
   )
 }
