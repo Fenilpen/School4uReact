2ce4ef00581ebd00289d7113556975c34596b65e
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-04 18:42:28 +0530

    Update App.jsx

--- Tailwind-css/src/App.jsx
+++ Tailwind-css/src/App.jsx
@@ -6,6 +6,21 @@ function App() {
 
   return (
     <> 
+    <div>
+    <header className=' flex justify-between text-xl text-black/80'>
+
+    <h1><span className='font-extrabold text-[tomato]'>B</span>rand</h1>
+    
+    <div className='flex gap-7'>
+      <a href="">Features</a>
+      <a href="">Use Cases</a>
+      <a href="">Intergrations</a>
+      <a href="">Portfolio</a>
+    </div>
+    
+    <button>Join US</button>
+    </header>
+    </div>
     </>
   )
 }
