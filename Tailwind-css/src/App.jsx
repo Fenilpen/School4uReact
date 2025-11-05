604f2361b34dc18fc5052b3a487723ebb1d47a58
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-05 18:21:17 +0530

    Update App.jsx

--- Tailwind-css/src/App.jsx
+++ Tailwind-css/src/App.jsx
@@ -6,12 +6,12 @@ function App() {
 
   return (
     <> 
-    <div>
+    <div className='bg-red-300 py-5' >
     <header className='max-w-[60rem] flex justify-between items-center text-xl text-black/80  bg-red-300 m-auto'>
 
-    <h1><span className='font-extrabold text-[tomato]'>B</span>rand</h1>
+    <h1 className='text-3xl' ><span className='font-extrabold text-[tomato]'>B</span>rand</h1>
     
-    <div className='flex gap-7'>
+    <div className='flex items-center gap-7'>
       <a href="">Features</a>
       <a href="">Use Cases</a>
       <a href="">Intergrations</a>
