285af7a87b941a208a03d8ecb5ca5f503c1e3b5a
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-06 17:58:58 +0530

    Update Header.jsx

--- Tailwind-css/src/Components/Header.jsx
+++ Tailwind-css/src/Components/Header.jsx
@@ -8,7 +8,12 @@ function Header() {
 
     <h1 className='text-3xl'><span className='font-extrabold text-[tomato]'>B</span>rand</h1>
     
-    
+    <div className='flex items-center gap-7'>
+      <a href="">Features</a>
+      <a href="">Use Cases</a>
+      <a href="">Intergrations</a>
+      <a href="">Portfolio</a>
+    </div>
     
     <button className='bg-[tomato] rounded-lg px-3 py-2'>Join US</button>
     </header>
