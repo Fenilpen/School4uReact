3702a790fc3eeb75e36c8265eedbe7fe9f1bcf8a
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-06 17:58:44 +0530

    Update Header.jsx

--- Tailwind-css/src/Components/Header.jsx
+++ Tailwind-css/src/Components/Header.jsx
@@ -1,1 +1,20 @@
-//
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='bg-red-300 py-5'>
+    
+    <header className='max-w-[60rem] flex justify-between items-center text-xl text-black/80 bg-red-300 m-auto'>
+
+    <h1 className='text-3xl'><span className='font-extrabold text-[tomato]'>B</span>rand</h1>
+    
+    
+    
+    <button className='bg-[tomato] rounded-lg px-3 py-2'>Join US</button>
+    </header>
+    
+    </div>
+  )
+}
+
+export default Header
