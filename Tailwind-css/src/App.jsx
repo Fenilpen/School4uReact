373d521f63dd33a24cd774912eb99b29a4e0e517
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-06 17:59:44 +0530

    Update App.jsx

--- Tailwind-css/src/App.jsx
+++ Tailwind-css/src/App.jsx
@@ -6,21 +6,10 @@ function App() {
 
   return (
     <> 
-    <div className='bg-red-300 py-5' >
-    <header className='max-w-[60rem] flex justify-between items-center text-xl text-black/80  bg-red-300 m-auto'>
-
-    <h1 className='text-3xl' ><span className='font-extrabold text-[tomato]'>B</span>rand</h1>
-    
-    <div className='flex items-center gap-7'>
-      <a href="">Features</a>
-      <a href="">Use Cases</a>
-      <a href="">Intergrations</a>
-      <a href="">Portfolio</a>
-    </div>
-    
-    <button className='bg-[tomato] rounded-lg px-3 py-2'> Join US</button>
-    </header>
-    </div>
+    <Header/>
+    <div className='max-w-[60rem] m-auto text-center py-[8rem]'>
+    <h1 className='text-7xl font-bold font-serif'>The best vay to
+    <span className='bg-red-300 rounded-2xl px-3'>Review</span> Creative Assets</h1>
     </>
   )
 }
