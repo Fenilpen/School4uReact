95b20b6ce25a50e49c7f66d8fd0ef42aeb52e563
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-07 12:03:45 +0530

    Update App.jsx

--- Tailwind-css/src/App.jsx
+++ Tailwind-css/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import "./Card.css"
 import Card from './Card'
+import Button from './Components/Button'
 import Header from './Components/Header'
 
 
@@ -12,6 +13,9 @@ function App() {
     <div className='max-w-[60rem] m-auto text-center py-[8rem]'>
     <h1 className='text-7xl font-bold font-serif'>The best vay to
     <span className='bg-red-300 rounded-2xl px-3'>Review</span> Creative Assets</h1>
+    <p className='text-lg'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quisquam quo laborum 
+    odio dolorem dignissimos deleniti ea explicabo praesentium consequatur?</p>
+    <Button title="Hello"/>
     </>
   )
 }
