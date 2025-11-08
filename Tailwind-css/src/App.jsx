8f2e12b5037a7cc2c4a8c012f64084bb3aae1dcf
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-08 18:17:16 +0530

    Update paragraph styling in App component

--- Tailwind-css/src/App.jsx
+++ Tailwind-css/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <div className='max-w-[60rem] m-auto text-center py-[8rem]'>
     <h1 className='text-7xl font-bold font-serif'>The best way to
     <span className='bg-red-300 rounded-2xl px-3'>Review</span> Creative Assets</h1>
-    <p className='text-lg'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quisquam quo laborum 
+    <p className='text-lg my-7'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quisquam quo laborum 
     odio dolorem dignissimos deleniti ea explicabo praesentium consequatur?</p>
     
     <div className='text-3xl'>
