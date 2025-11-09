2ed39aeac7bf6356cd18cef1c16ddac9f710e72e
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-09 18:33:30 +0530

    Update Hero.jsx

--- Component/src/Components/Hero.jsx
+++ Component/src/Components/Hero.jsx
@@ -1,1 +1,16 @@
-//
+import React from 'react'
+
+function Hero() {
+  return (
+    <>
+    <div className='max-w-[60rem] m-auto text-center py-[8rem]'>
+    <h1 className='text-7xl font-bold font-serif'>The best way to
+    <span className='bg-red-300 rounded-2xl px-3'>Review</span> Creative Assets</h1>
+    <p className='text-lg my-7' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quisquam quo laborum 
+    odio dolorem dignissimos deleniti ea explicabo praesentium consequatur?</p>
+    </div>
+    </>
+  )
+}
+
+export default Hero
