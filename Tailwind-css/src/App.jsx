a1b202dd04ea3dc8861d5688d81cfaf1438ab794
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-11 18:23:20 +0530

    Update App.jsx

--- Tailwind-css/src/App.jsx
+++ Tailwind-css/src/App.jsx
@@ -10,15 +10,11 @@ function App() {
   return (
     <> 
     <Header/>
-    <div className='max-w-[60rem] m-auto text-center py-[8rem]'>
-    <h1 className='text-7xl font-bold font-serif'>The best way to
-    <span className='bg-red-300 rounded-2xl px-3'>Review</span> Creative Assets</h1>
-    <p className='text-lg my-7'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quisquam quo laborum 
-    odio dolorem dignissimos deleniti ea explicabo praesentium consequatur?</p>
-    
+    <Hero/>
     <div className='text-3xl'>
     <Button title="Join subscription"/>
     </div>
+    <Footer/>
     </>
   )
 }
