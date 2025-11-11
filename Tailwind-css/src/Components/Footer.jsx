8091ca79f078f5f85a1d679465e96bd14dd24fa3
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-11 18:22:38 +0530

    Refactor Footer component structure and styling

--- Tailwind-css/src/Components/Footer.jsx
+++ Tailwind-css/src/Components/Footer.jsx
@@ -1,7 +1,24 @@
+import Button from './Button'
+
+
 function Footer() {
   return (
-    <div>
-      
+    <div className='bg-red-300 py-5'>
+    
+    <header className='max-w-[60rem] flex justify-between items-center text-xl text-black/80 bg-red-300 m-auto'>
+
+    <h1 className='text-3xl'><span className='font-extrabold text-[tomato]'>B</span>rand</h1>
+    
+    <div className='flex items-center gap-7'>
+      <a href="">Features</a>
+      <a href="">Use Cases</a>
+      <a href="">Intergrations</a>
+      <a href="">Portfolio</a>
+    </div>
+    
+    <Button title="Join-US"/>
+    </header>
+    
     </div>
   )
 }
