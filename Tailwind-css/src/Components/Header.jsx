dbfbdc2d42fb8d0e4f46cd163c6d26e534685cce
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-13 18:06:00 +0530

    Update Header.jsx

--- Tailwind-css/src/Components/Header.jsx
+++ Tailwind-css/src/Components/Header.jsx
@@ -6,11 +6,11 @@ function Header() {
   return (
     <div className='bg-red-300 py-5'>
     
-    <header className='max-w-[60rem] flex justify-between items-center text-xl text-black/80 bg-red-300 m-auto'>
+    <header  className="px-[1rem] max-w-[60rem] flex items-center justify-between text-xl text-black/80 m-auto">
 
     <h1 className='text-3xl'><span className='font-extrabold text-[tomato]'>B</span>rand</h1>
     
-    <div className='flex items-center gap-7'>
+    <div className='max-[770px]:hidden flex items-center gap-7'>
       <a href="">Features</a>
       <a href="">Use Cases</a>
       <a href="">Intergrations</a>
