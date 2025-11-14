958b0dd3c00426bf7f8a614b360602d7d0ddc7be
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-14 18:07:57 +0530

    Update Hero.jsx

--- Tailwind-css/src/Components/Hero.jsx
+++ Tailwind-css/src/Components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <>
     <div className='max-w-[60rem] m-auto text-center py-[8rem]'>
-    <h1 className='text-7xl font-bold font-serif'>The best way to
+    <h1 className='max-[500px]:text3xl text-7xl font-bold font-serif'>The best way to
     <span className='bg-red-300 rounded-2xl px-3'>Review</span> Creative Assets</h1>
     <p className='text-lg my-7' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quisquam quo laborum 
     odio dolorem dignissimos deleniti ea explicabo praesentium consequatur?</p>
