3bf277850d80e158a9a6abfd7d3344e0214fc64a
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-14 18:10:33 +0530

    Update Hero.jsx

--- Tailwind-css/src/Components/Hero.jsx
+++ Tailwind-css/src/Components/Hero.jsx
@@ -4,7 +4,7 @@ import Button from './Button'
 function Hero() {
   return (
     <>
-    <div className='max-w-[60rem] m-auto text-center py-[8rem]'>
+    <div className='px-[1rem] max-w-[60rem] m-auto text-center py-[8rem]'>
     <h1 className='max-[500px]:text3xl text-7xl font-bold font-serif'>The best way to
     <span className='bg-red-300 rounded-2xl px-3'>Review</span> Creative Assets</h1>
     <p className='text-lg my-7' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel quisquam quo laborum 
