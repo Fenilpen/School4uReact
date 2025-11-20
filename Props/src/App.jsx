8b6bf6ecee4a97e46d902291c63258650e4ab3fb
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-20 17:32:14 +0530

    Update App.jsx

--- Props/src/App.jsx
+++ Props/src/App.jsx
@@ -8,14 +8,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     <div className='layout'>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    </div>
-    </>
+    
+     
   )
 }
 
