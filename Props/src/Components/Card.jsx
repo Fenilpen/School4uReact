411d45a0aec5426ac1afd679d6182e36a0eadb8b
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-11-28 18:15:50 +0530

    Update Card.jsx

--- Props/src/Components/Card.jsx
+++ Props/src/Components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Card({data,isStudent}) {
+function Card({data,isStudent,content}) {
 
 const {name,age,quali} = data
 console.log(name,age,quali,isStudent);
   
   return (
     <div className='card'>
-    
+     {content}
     </div>
   )
 }
