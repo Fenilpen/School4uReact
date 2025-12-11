c90f10be53a032d3e144bea59b3bcd3b041df830
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-12-11 17:44:53 +0530

    Add Header component to App

--- Dataflow/src/App.jsx
+++ Dataflow/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Header/>
+    <Header/>
     <Footer/>
     </>
   )
