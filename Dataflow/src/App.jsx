f6a40df26f9fb3b2f1927cc81077f9c810c1a77a
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-12-15 17:48:20 +0530

    Add Layout component to App component

--- Dataflow/src/App.jsx
+++ Dataflow/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Layout from './components/Layout'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
     <Header/>
+    <Layout/>
     <Footer/>
     </>
   )
