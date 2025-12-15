880bec59746798f594729810a5f9e7150dd77ea7
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-12-15 17:49:10 +0530

    Increase height of Layout component

--- Dataflow/src/Components/Layout.jsx
+++ Dataflow/src/Components/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Layout() {
   return (
-    <div className='h-[10vw] bg-gray-400w-full'>
+    <div className='h-[30vw] bg-gray-400w-full'>
       Layout
     </div>
   )
