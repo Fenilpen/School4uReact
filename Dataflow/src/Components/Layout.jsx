e673140895f90ef415beb55eea4c21989e43d4d7
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-12-20 18:37:40 +0530

    Add props parameter to Layout component

--- Dataflow/src/Components/Layout.jsx
+++ Dataflow/src/Components/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from './Hero'
 import About from './About'
 
-function Layout() {
+function Layout(props) {
   return (
     <div className='p-[2rem] h-[30vh] bg-gray-400w-full'>
       Layout
