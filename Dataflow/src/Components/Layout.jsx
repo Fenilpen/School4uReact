a5198b9059b96f6d2f0b52c9363084a6adbaeedb
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-12-22 17:41:19 +0530

    Update Layout.jsx

--- Dataflow/src/Components/Layout.jsx
+++ Dataflow/src/Components/Layout.jsx
@@ -3,6 +3,7 @@ import Hero from './Hero'
 import About from './About'
 
 function Layout(props) {
+  const [count,setCount] = useState(11)
   return (
     <div className='p-[2rem] h-[30vh] bg-gray-400w-full'>
       Layout
