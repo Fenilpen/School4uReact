c0e3ab6b5fe291b25c2fc7bef9d8d33fd883557a
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2025-12-23 18:08:53 +0530

    Update App.jsx

--- Dataflow/src/App.jsx
+++ Dataflow/src/App.jsx
@@ -7,9 +7,13 @@ import Layout from './components/Layout'
 function App() {
 const [data,setData] = useState(0);
 
+function recieve (param) {
+console.log(param);
+}
+
   return (
     <>
-      <Header data={data}/>
+      <Header data={data} />
       <Layout data={data}/>
       <Footer data={data}/>
     </>
