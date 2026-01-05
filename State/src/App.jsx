c4315d7f90cb890fbe5256f6ac33d3dfad530606
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-01-05 18:21:23 +0530

    Add Card component to App

--- State/src/App.jsx
+++ State/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './Card'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -20,6 +21,7 @@ function App() {
     <>
      <h1>{counts}</h1>
      <button onClick={increment}> plus </button>
+      <Card/>
     </>
   )
 }
