581de7d309a127f8b3fef149e67fd0d845d04f04
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-01-19 18:17:37 +0530

    Update App.jsx

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -11,3 +11,9 @@ function App() {
 }
 
 export default App
+
+// what is hooks
+// hooks are one of the most powerfull fetures in react
+// they were introduced in react 16.8 to let us use state and lifecycle methods inside
+// functional components - without writing a class
+
