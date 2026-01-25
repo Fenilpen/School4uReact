3e6fb8a64bc002ba9ea8f1b725969069edd9e56e
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-01-25 19:00:33 +0530

    Update App.jsx

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+ const [count, setCount] = useState(0)
  useEffect(()=>{
     console.log("hi in useEffect") 
   },[])
