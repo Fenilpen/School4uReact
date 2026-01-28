9bf9aec245e192f50b8bad28cdf34625a0ab9ce0
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-01-28 18:42:29 +0530

    Update App.jsx

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -2,16 +2,17 @@ import {useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
- const [count, setCount] = useState(0)
- const [decVal,setDecVal] = useState(0)
+const [count, setCount] = useState(0)
+const [decVal,setDecVal] = useState(0)
  
  useEffect(()=>{
     console.log("hi in useEffect") 
+   // return()
   },[count,decVal])
 
   return (
     <>
-      <h1>{count}</h1>
+     <h1>{count}</h1>
      <button onClick={()=>{
       setCount(count + 1)
      }}>decrement</button>
