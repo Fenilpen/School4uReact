84bb7edb10d223e6adcc4316a6f79748a28b0aab
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-02-02 18:00:24 +0530

    Update App.jsx

--- Colorswitcher/src/App.jsx
+++ Colorswitcher/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Button from './Components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h1 className='bg-red-400'>Color switcher</h1>
+      <Button/>
     </>
   )
 }
