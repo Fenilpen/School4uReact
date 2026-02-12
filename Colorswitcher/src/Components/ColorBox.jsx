53238c03baee426c8d5e310137c3d86ee62cd9a9
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-02-12 15:45:28 +0530

    Update ColorBox.jsx

--- Colorswitcher/src/Components/ColorBox.jsx
+++ Colorswitcher/src/Components/ColorBox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ColorBox = () => {
   return (
-    <div className='w-[300px] h-[300px] bg-[red]  rounded-2xl'>
+    <div className='w-[300px] h-[300px] bg-[red]  rounded-2xl font-extrabold'>
       
     </div>
   )
