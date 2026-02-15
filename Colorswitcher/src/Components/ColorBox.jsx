61dd883301318627f126c92a35450c6a69e45775
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-02-15 18:16:51 +0530

    Update ColorBox.jsx

--- Colorswitcher/src/Components/ColorBox.jsx
+++ Colorswitcher/src/Components/ColorBox.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ColorBox = () => {
   return (
-    <div className='m-auto w-[300px] h-[300px] bg-[red]  rounded-2xl font-extrabold text-center content-center border-2'>
+    <div 
+    style={
+      {backgroundcolor: color}
+    }
+    className='m-auto w-[300px] h-[300px] bg-[red]  rounded-2xl font-extrabold text-center content-center border-2'>
       
     </div>
   )
