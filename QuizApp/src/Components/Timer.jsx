a94563be6dac0b54dc824a29dd62de76ee091f04
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-02-25 17:39:55 +0530

    Update Timer.jsx

--- QuizApp/src/Components/Timer.jsx
+++ QuizApp/src/Components/Timer.jsx
@@ -3,6 +3,14 @@ import React from 'react'
 function Timer() {
   
   const [leftTime,setLeftTime] = useState(5)
+
+  useEffect(()=>{
+
+    setInterval(() => {
+
+    },1000)
+    
+  },[])
   
   return (
     <div>
