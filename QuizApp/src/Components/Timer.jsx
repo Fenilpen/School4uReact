17b8700560e55ee8efd181579cf4563cb9c8149a
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-02-25 17:37:19 +0530

    Update Timer.jsx

--- QuizApp/src/Components/Timer.jsx
+++ QuizApp/src/Components/Timer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 function Timer() {
+  
+  const [leftTime,setLeftTime] = useState(5)
+  
   return (
     <div>
-      
+      Time Left: {leftTime}
     </div>
   )
 }
