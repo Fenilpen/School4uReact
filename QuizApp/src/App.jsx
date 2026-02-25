72615c453e9cea6ab2ccc8e753ad71d88a24a614
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-02-25 17:33:33 +0530

    Update App.jsx

--- QuizApp/src/App.jsx
+++ QuizApp/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Timer from './Components/Timer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    <Timer/>
     </>
   )
 }
