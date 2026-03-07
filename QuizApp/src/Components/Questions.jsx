b78532492b38ffb7ee35df4498438e9045f1fad9
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-03-07 16:59:32 +0530

    Update Questions.jsx

--- QuizApp/src/Components/Questions.jsx
+++ QuizApp/src/Components/Questions.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import questions from "./Question.json"
 
 function Questions() {
+  const [currentIndex, setIndex] = useState(0)
+  
   return (
     <div>
       Question
