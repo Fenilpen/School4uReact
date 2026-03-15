7c643764847868541bf429407bb62e75041e7f4a
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-03-15 18:00:01 +0530

    Import Questions component in App.jsx

--- QuizApp/src/App.jsx
+++ QuizApp/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Timer from './Components/Timer'
+import Questions from './Components/Questions'
 
 function App() {
   const [count, setCount] = useState(0)
