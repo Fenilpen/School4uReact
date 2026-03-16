4d33fde31cf946e090aa2f80ed497f4217064bac
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-03-16 17:47:00 +0530

    Update App.jsx

--- QuizApp/src/App.jsx
+++ QuizApp/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Timer from './Components/Timer'
 import Questions from './Components/Questions'
+import Result from './Components/Result'
 
 function App() {
   const [isOver,setIsOver] = useState(false)
