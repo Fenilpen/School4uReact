3f5df3441045ae8f4d557bafb5143317b72c43f0
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-03-20 19:15:31 +0530

    Fix logic for advancing question index and ending quiz

--- QuizApp/src/Components/Questions.jsx
+++ QuizApp/src/Components/Questions.jsx
@@ -12,7 +12,8 @@ function Questions({setIsOver}) {
   }
    if(currentIndex < questions.length - 1){
        setCurrentIndex(prev = prev + 1)
-    }
+    }else{
+     setIsOver()
   }
   
   return (
