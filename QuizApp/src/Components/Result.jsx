776938e3db587780ac3a653519645ce1d70be54d
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-03-28 14:44:02 +0530

    Update Result.jsx

--- QuizApp/src/Components/Result.jsx
+++ QuizApp/src/Components/Result.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Result({score}) {
   return (
+    <>
     <div> result </div>
-      <h2>your score is {score}</h2>
+    <h2>your score is {score}</h2>
+    </>
   )
 }
 
