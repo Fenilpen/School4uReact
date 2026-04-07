4001496e8139e87e494981c6c83f112e26c171ab
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-04-07 18:11:57 +0530

    Update App.jsx

--- EventHandling/src/App.jsx
+++ EventHandling/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     alert("Hello Fenil Patel")
   }}>Click me</button>
 
-    <input className='border border-white' type='text' placeholder='Enter name'/>      
+    <input className='border border-white' type='text' onChange={} placeholder='Enter name'/>      
     </>
   )
 }
