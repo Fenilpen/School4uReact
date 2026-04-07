79f05d67b44da34b56b34cd6bd0bd2ec2959d298
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-04-07 18:11:24 +0530

    Update App.jsx

--- EventHandling/src/App.jsx
+++ EventHandling/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 
 function App() {
   
-  function greet(){
-    alert("Hello Fenil Patel")
-  }
+  
   
   return (
     <>
     <h1 className='bg-red-500'>Hello Universe</h1>
-    <button onClick={greet}>Click me</button>
+    <button onClick={function greet(){
+    alert("Hello Fenil Patel")
+  }}>Click me</button>
 
     <input className='border border-white' type='text' placeholder='Enter name'/>      
     </>
