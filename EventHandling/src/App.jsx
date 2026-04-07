36ad089f0d6dfb083ac19016f34fc069e09d859d
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-04-07 18:09:35 +0530

    Update App.jsx

--- EventHandling/src/App.jsx
+++ EventHandling/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <h1 className='bg-red-500'>Hello Universe</h1>
     <button onClick={greet}>Click me</button>
 
-    <input className='border border-white' type='text'/>      
+    <input className='border border-white' type='text' placeholder='Enter name'/>      
     </>
   )
 }
