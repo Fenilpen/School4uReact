0dc5d4e475cc6077ff8aeb36c4f55b9f58d62910
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-05-06 17:36:55 +0530

    Create Ifcomponents.jsx

---
+++ ConditionalRendering/src/Components/Ifcomponents.jsx
@@ -0,0 +1,42 @@
+function Ifcomponent(){
+
+const option = "a"
+
+
+if(option === "a"){
+  return(
+  <div>
+    <h1>component A</h1>
+  </div>
+
+) }
+
+if(option === "b"){
+  return(
+  <div>
+    <h1>component B</h1>
+  </div>
+
+) }
+
+
+if(option === "c"){
+  return(
+  <div>
+    <h1>component C</h1>
+  </div>
+
+) }
+
+
+if(option === "d"){
+  return(
+  <div>
+    <h1>component D</h1>
+  </div>
+
+) }
+
+}
+
+export default Ifcomponent
