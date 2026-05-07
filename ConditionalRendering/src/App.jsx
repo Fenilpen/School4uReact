e53403aeecf9ff5b8e6bb887a466abd6ff8caa84
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-05-07 18:15:27 +0530

    Update App.jsx

--- ConditionalRendering/src/App.jsx
+++ ConditionalRendering/src/App.jsx
@@ -1,42 +1,6 @@
 import { useState } from 'react'
 
 function App() {
-
-  // const isLogin = true  
-  const option = "a"
-
-  if(option === "a"){
-  return(
-  <div>
-    <h1>component A</h1>
-  </div>
-) }
-
-if(option === "b"){
-  return(
-  <div>
-    <h1>component B</h1>
-  </div>
-
-) }
-
-
-if(option === "c"){
-  return(
-  <div>
-    <h1>component C</h1>
-  </div>
-
-) }
-
-
-if(option === "d"){
-  return(
-  <div>
-    <h1>component D</h1>
-  </div>
-
-) }
   
 
 return(
