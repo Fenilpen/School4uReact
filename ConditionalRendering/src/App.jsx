d40ac4082ba321927407518566e6b3985f845732
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-05-11 18:52:31 +0530

    Update App.jsx

--- ConditionalRendering/src/App.jsx
+++ ConditionalRendering/src/App.jsx
@@ -9,7 +9,7 @@ return(
   <div>
   {option === "a" ? <h1>Component A</h1> : 
     
-  (option === "b" ? <h1> components B </h1> : <h1>Other components</h1> )}
+ (option === "b" ? <h1> components B </h1> : ( option === "c" ? <h1>components C</h1> : <h1>Other Components</h1> ))}
     
   </div>
 )
