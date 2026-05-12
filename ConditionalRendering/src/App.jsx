d71dc680b7197a93b779ba393adf319637ee04f2
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-05-12 22:50:45 +0530

    Update App.jsx

--- ConditionalRendering/src/App.jsx
+++ ConditionalRendering/src/App.jsx
@@ -5,11 +5,7 @@ const App = () => {
   const hasMessege = "hi india how are you"
   
   return (
-    <div>
-      {
-        hasMessege ? hasMessege : "messege is not there"
-      }
-    </div>
+    
   )
 }
 
