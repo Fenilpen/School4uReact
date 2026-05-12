4815accffbf42be2b945aa2d6160d32944ba7d6a
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-05-12 19:04:14 +0530

    Update App.jsx

--- ConditionalRendering/src/App.jsx
+++ ConditionalRendering/src/App.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const App = () => {
 
+  const hasMessege = "hi india how are you"
+  
   return (
     <div>
-      
+      {
+        hasMessege ? hasMessege : "messege is not there"
+      }
     </div>
   )
 }
