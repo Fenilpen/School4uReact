0cee20193e0ab6a5e5e3ea8db13737b7acb40c60
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-05-12 18:59:14 +0530

    Update App.jsx

--- ConditionalRendering/src/App.jsx
+++ ConditionalRendering/src/App.jsx
@@ -1,17 +1,1 @@
-import { useState } from 'react'
-
-function App() {
-  
-const isLogin = true
-const option = "a";
-  
-return(
-  <div>
-  {option === "a" ? <h1>Component A</h1> : 
-    
- (option === "b" ? <h1> components B </h1> : ( option === "c" ? <h1>components C</h1> : <h1>Other Components</h1> ))}
-    
-  </div>
-)
-
-export default App
+//
