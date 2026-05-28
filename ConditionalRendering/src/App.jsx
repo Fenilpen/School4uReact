b7ad292e44aa31e9dc078dcaf77275ec86126912
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-05-28 17:38:36 +0530

    Update App.jsx

--- ConditionalRendering/src/App.jsx
+++ ConditionalRendering/src/App.jsx
@@ -1,32 +1,7 @@
 import React from 'react'
-import ComponentA from './Components/ComponentA'
-import ComponentB from './Components/ComponentB'
-import ComponentC from './Components/ComponentC'
 
 const App = () => {
-
-const option = "a"
-const content = <h1>hello how are you</h1>
-
-
-  if(option === "a"){
-        content = <ComponentA/>
-    }else if (option === "b" ){
-        content = <ComponentB/>
-    }else if (option === "c") {
-        content = <ComponentC/>
-    }else {
-        content = <h1>Other component</h1>
-    }
   
-    return(
-        <>
-        <div>{content}</div>
-        </>
-    )
 }
 
 export default App
-
-// store JSX in a variable
-// helps keep return statement clean and readable
