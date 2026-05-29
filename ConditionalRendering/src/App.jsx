3342702115301318a8f4e44b6f072f3ba84bf0e1
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-05-29 17:54:54 +0530

    Update App.jsx

--- ConditionalRendering/src/App.jsx
+++ ConditionalRendering/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const App = () => {
+    const color = "red"
+
+    return (
     <div>
       <h1 className={`${color ==="red" ? 'bg-red-500' : 'bg-blue-600' }`}>Hello how are you</h1>
     </div>
+        )
 }
 
 export default App
