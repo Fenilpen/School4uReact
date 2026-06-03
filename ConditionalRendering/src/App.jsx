3ec794e5f9f1c9d5774b029308c65a17b46efacd
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-06-03 17:30:10 +0530

    Update App.jsx

--- ConditionalRendering/src/App.jsx
+++ ConditionalRendering/src/App.jsx
@@ -2,32 +2,7 @@ import React from 'react'
 
 const App = () => {
     
-    const [toggle,setToggle] = useState(false);
-    if(!toggle) return null;
-    
-    return (
-        
-    <>
-        
-      {/* <h1 className={`${color ==="red" ? 'bg-red-500' : 'bg-blue-600' }`}>Hello how are you</h1> */}
-        {
-            toggle ? (
-                <h1>this is the message</h1>
-            ) : (
-                <h1>toggle is false</h1>
-            )
-        }
-
-        <button onClick={()=>{
-          setToggle(!Toggle)
-    }}></button>
-        
-    </>
-        
-        )
+    return ()
 }
 
 export default App
-
-// returning null means rendering nothing
-// commom in modal or popup components
