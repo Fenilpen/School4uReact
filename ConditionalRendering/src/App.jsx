95f31fc7ed3a31f8f20e3b44560c3712280a55c5
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-06-04 15:34:45 +0530

    Update App.jsx

--- ConditionalRendering/src/App.jsx
+++ ConditionalRendering/src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const App = () => {
+
+import ComponentA from './Components'
+import ComponentB from './Components'
+import ComponentC from './Components'
+
     
     return ()
 }
