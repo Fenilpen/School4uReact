e7b22bfea7cef3845b4722cc0a3e6e9c92f966d0
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-06-05 15:09:02 +0530

    Update App.jsx

--- ConditionalRendering/src/App.jsx
+++ ConditionalRendering/src/App.jsx
@@ -1,16 +1,1 @@
-import React from 'react'
-import ComponentA from './Components'
-import ComponentB from './Components'
-import ComponentC from './Components'
-
-const App = () => {
-
-  const Component = ComponentA
-   const Component = option === "a" ? ComponentA : ComponentB
-  
-    return (
-        
-    )
-}
-
-export default App
+//
