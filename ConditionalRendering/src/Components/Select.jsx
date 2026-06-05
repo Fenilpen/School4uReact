b4584d8223b87cf8168911386e37944e90937f02
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-06-05 15:08:27 +0530

    Update Select.jsx

--- ConditionalRendering/src/Components/Select.jsx
+++ ConditionalRendering/src/Components/Select.jsx
@@ -1,1 +1,18 @@
-//
+import React from 'react'
+import ComponentA from './Components'
+import ComponentB from './Components'
+import ComponentC from './Components'
+
+function Select() {
+
+  const option = 'a'
+  const Component = option === "a" ? ComponentA : ComponentB
+
+  return (
+    <div>
+      <Component/>
+    </div>
+  )
+}
+
+export default Select
