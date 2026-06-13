3c3d4d4ea2ec909425cb17a7212ccad8c7620772
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-06-13 18:25:48 +0530

    Update App.jsx

--- ConditionalRendering/src/App.jsx
+++ ConditionalRendering/src/App.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const App = () => {
+  
+  const status = {
+    loading: <h1>Loading.....</h1>,
+    error: <h1>eroor aa gaya</h1>,
+    success: <h1>successfull</h1>
+  }
+  
   return (
     <div>
       
