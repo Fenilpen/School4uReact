e2a6c3d654e0d2c185bfadda65d866e740612732
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-07-01 16:50:05 +0530

    Update App.jsx

--- Conditional-practice/src/App.jsx
+++ Conditional-practice/src/App.jsx
@@ -18,6 +18,10 @@ function App() {
         <div className='w-full h-full max-w-[40rem] max-h-[20rem] bg-[#242424]'>
       <h1>confirm</h1>  
       <p>are you sure you want to delete this</p>
+      <div>
+        <button></button>
+        <button></button>
+      </div>
       </div>
       </div>
     )}
