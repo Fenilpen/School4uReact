55e2461a5247587297635447a843556c3ff3aaad
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-07-07 17:30:17 +0530

    Update App.jsx

--- Conditional-practice/src/App.jsx
+++ Conditional-practice/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <div className='absolute top-0 left-0 w-screen h-screen bg-black/50 items-center justify-center flex-col'>
       <button onClick={()=>{
           setPopup(false)
-        }} className='absolute top-[1rem] right-[1rem]'>Cancel</button>
+        }} className='bg-[#242424] absolute top-[1rem] right-[1rem]'>Cancel</button>
         <div className='w-full h-full max-w-[40rem] max-h-[20rem] bg-[#242424] p-[5] flex items-center'>
       <h1 className='text-2xl'>confirm</h1>  
       <pclassName='text-lg my-5'>are you sure you want to delete this</p>
