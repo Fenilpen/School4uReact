e94f069af8383be7581a58d06b0deb09c4b75f16
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-07-17 12:02:22 +0530

    Update Popup.jsx

--- Conditional-practice/src/Popup.jsx
+++ Conditional-practice/src/Popup.jsx
@@ -12,8 +12,8 @@ function Popup({popup,setPopup,title,description}) {
         }} className='bg-[#242424] absolute top-[1rem] right-[1rem]'>Cancel</button>
       <div className='w-full h-full max-w-[40rem] max-h-[20rem] bg-[#242424] rounded-2xl p-[5] flex items-center flex-col'>
 
-      <h1 className='text-2xl'>confirm</h1>  
-      <p className='text-lg my-5'>are you sure you want to delete this</p>
+      <h1 className='text-2xl'>{title}</h1>  
+      <p className='text-lg my-5'>{description}</p>
 
       <div className='flex gap-10'>
         <button className='bg-[tomato]'>No</button>
