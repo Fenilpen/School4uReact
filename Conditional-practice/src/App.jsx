2ff8449b05508f9104755eb76e6b40a819943335
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-07-21 18:26:45 +0530

    Update App.jsx

--- Conditional-practice/src/App.jsx
+++ Conditional-practice/src/App.jsx
@@ -7,31 +7,30 @@ const [popup, setPopup] = useState(false)
 const [editPopup,setEditPopup] = useState(false)
   
   return (
-  <button className='bg-black' onClick={()=>{
+    <>
+    {/* {delete} */}
+    <button className='bg-black' onClick={()=>{
       setPopup(true)
     }}>delete</button>
-
-
-  <button className='bg-black' onClick={()=>{
-      setPopup(true)
-    }}>edit</button>
     
-    <Popup 
-    popup={popup} 
-    setPopup={setPopup}/>
-    edit={"edit"}
-    description={"are you sure you want to edit this"}
-
+    <Popup popup={popup} 
+    setPopup={setPopup} 
+    title={"delete"} 
+    description={"are you sure you want to delete this?"}/>
+    
     {/* {edit} */}
     <button className='bg-black' onClick={()=>{
       setPopup(true)
     }}>Edit</button>
+    
+    <Popup 
+    popup={popup} 
+    setPopup={setPopup}
+    title={"edit"}
+    description={"are you sure you want to edit this"}
+    />
 
-   <Popup popup={popup} 
-    setPopup={setPopup} 
-    title={"delete"} 
-    description={"are you sure you want to delete this?"}/>
-  
+    </>
   )
 
 }
