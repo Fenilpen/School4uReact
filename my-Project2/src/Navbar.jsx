0c6be4820ee3f2ad9c55c9ed1b6099cc92d8b1c7
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-07-25 17:34:07 +0530

    Update Navbar.jsx

--- my-Project2/src/Navbar.jsx
+++ my-Project2/src/Navbar.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-      
+      <h1>Logo</h1>
+      <div>
+        <a href="">home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+        <a href="">Service</a>
+        <a href="">Help</a>
+      </div>
     </div>
   )
 }
