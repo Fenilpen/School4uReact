c6630b044412e60fc529c1e38be81f2c48e87139
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-08-04 18:37:33 +0530

    Update Navbar.jsx

--- my-Project2/src/Navbar.jsx
+++ my-Project2/src/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='w-full flex justify-between items-center px-[1rem] py-[2rem] bg-[lightseagreen]'>
-      <h1 className='text-2xl font-bold' >Logo</h1>
+      <h1 className='text-2xl font-bold'>Logo</h1>
       
-      <div className='flex gap-3 items-center text-lg' >
+      <div className='flex gap-9 items-center text-lg' >
         <a href="">home</a>
         <a href="">About</a>
         <a href="">Contact</a>
