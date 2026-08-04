ec50e6f1e110feaf4fb0debed499273216d714d2
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-08-04 18:36:15 +0530

    Update Navbar.jsx

--- my-Project2/src/Navbar.jsx
+++ my-Project2/src/Navbar.jsx
@@ -4,7 +4,8 @@ function Navbar() {
   return (
     <div className='w-full flex justify-between items-center px-[1rem] py-[2rem] bg-[lightseagreen]'>
       <h1 className='text-2xl font-bold' >Logo</h1>
-      <div className='flex gap-3 items-center' >
+      
+      <div className='flex gap-3 items-center text-lg' >
         <a href="">home</a>
         <a href="">About</a>
         <a href="">Contact</a>
