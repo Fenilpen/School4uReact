a352ddee8c348593e005064ab1828a6cdf7a6cc2
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-08-07 16:50:35 +0530

    Update Navbar.jsx

--- my-Project2/src/Navbar.jsx
+++ my-Project2/src/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
       <div className='text-2xl flex items-center gap-5'>
         <span></span>
         <span></span>
-        <span></span>
+        <span className='min-[720px]: hidden' ></span>
       </div>
       
     </div>
