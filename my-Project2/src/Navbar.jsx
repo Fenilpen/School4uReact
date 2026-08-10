31a75252a6661e3985246807464f72cbd575a043
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-08-10 17:17:25 +0530

    Change mobile navbar background to full height

--- my-Project2/src/Navbar.jsx
+++ my-Project2/src/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
       </div>
 
 {/* Mobile view */}
-      <div className='absolute bg-[red] flex gap-9 items-center text-lg max-[720px]:hidden'>
+      <div className='absolute bg-[red] h-screen flex gap-9 items-center text-lg max-[720px]:hidden'>
         <a href="">home</a>
         <a href="">About</a>
         <a href="">Contact</a>
