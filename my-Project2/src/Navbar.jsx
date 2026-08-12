0f8053a5eb65abe3fc98ea2d96db1fe4ff7ff8b9
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-08-12 16:34:58 +0530

    Update Navbar.jsx

--- my-Project2/src/Navbar.jsx
+++ my-Project2/src/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
       </div>
 
 {/* Mobile view */}
-      <div className='absolute bg-[black] h-screen top-0 left-0 w-[20rem] flex gap-9 items-center text-lg max-[720px]:hidden'>
+      <div className='absolute bg-[black] h-screen top-0 left-0 w-[20rem] flex'>
         <a href="">home</a>
         <a href="">About</a>
         <a href="">Contact</a>
