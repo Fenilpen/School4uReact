343cfd98c951a245492ce98d732c07628684a4bc
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-08-14 18:24:44 +0530

    Update Navbar.jsx

--- my-Project2/src/Navbar.jsx
+++ my-Project2/src/Navbar.jsx
@@ -21,7 +21,7 @@ function Navbar() {
 
 {/* Mobile view */}
       <div className='absolute bg-[black] h-screen top-0 left-0 w-[20rem] flex'>
-        <a href="" className='bg-red-200 p-5 flex items-center justify-center' >home</a>
+        <a href="" className='hover bg-[black] p-5 flex items-center justify-center' >home</a>
         <a href="">About</a>
         <a href="">Contact</a>
         <a href="">Service</a>
