31ceba38170f7856ef5b76692cdbebd8adc386b8
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-08-25 14:37:00 +0530

    Update Navbar.jsx

--- my-Project2/src/Navbar.jsx
+++ my-Project2/src/Navbar.jsx
@@ -25,7 +25,7 @@ function Navbar() {
       </div>
 
 {/* Mobile view */}
-      <div className={`absolute bg-[black] h-screen top-0 left-0 w-[20rem] flex flex-col translate-x-[100%] ${mobileview ? 'translate-x-0' : "translate-x-[100%]"}`} transition-all duration-150>
+      <div className={`absolute bg-[black] h-screen top-0 left-0 w-[20rem] flex flex-col translate-x-[100%] ${mobileview ? 'translate-x-0' : "translate-x-[100%]"}`} transition-all duration-300>
         <button 
         onClick={()=>{
           setMobileview(mobileview)
