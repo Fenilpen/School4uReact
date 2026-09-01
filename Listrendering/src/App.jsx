6ae8f24c78b5e78d24207b22aa5815701234b948
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-09-01 18:53:11 +0530

    Update App.jsx

--- Listrendering/src/App.jsx
+++ Listrendering/src/App.jsx
@@ -25,3 +25,9 @@ export default App
 // <ul> {fruits.map((name,i)=> <li key={i}>{name}</li>)}  </ul>
 
 // (B) Array of objects
+// const users  = {
+   // {id: 1, name: "one",age :21}
+   // {id: 1, name: "two",age :22}
+   // {id: 1, name: "three",age :23}
+//}
+
