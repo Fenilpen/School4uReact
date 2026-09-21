2baca8dc8b1036c6234cc5734b2f136c86e9f6e0
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-09-21 16:51:27 +0530

    Add margin-top to Usercard background

--- Listrendering/src/Usercard.jsx
+++ Listrendering/src/Usercard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Usercard = ({userDetails}) => {
   return (
-    <div className='[bg-tomato] >
+    <div className='[bg-tomato] mt-5' >
       <h1>{userDetails.id}</h1>
       <h1>{userDetails.name}</h1>
       <h1>{userDetails.age}</h1>
