03a7950a3609bced169a84bd7026fd3b1546768b
Fenilpen <144175245+Fenilpen@users.noreply.github.com>
2026-09-23 17:03:43 +0530

    Update Usercard.jsx

--- Listrendering/src/Usercard.jsx
+++ Listrendering/src/Usercard.jsx
@@ -4,7 +4,7 @@ const Usercard = ({userDetails}) => {
   return (
     <div className='[bg-tomato] mt-5' >
       <h1>{userDetails.id}</h1>
-      <h1>{userDetails.name}</h1>
+      <h1 {userDetails.age > 18 ? userDetails.name : "to young"} >{userDetails.name}</h1>
       <h1>{userDetails.age}</h1>
     </div>
   )
